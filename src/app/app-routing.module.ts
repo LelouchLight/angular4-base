3832acee027eb70da38e0337c242e7331aeb6c03
Matthew <the.eclectic.futurist@gmail.com>
2017-09-04 19:47:21 -0700

    v 1.0

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes, PreloadAllModules  } from '@angular/router';
+
+import { HomePageComponent } from './home-page/home-page.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules}) ],
+  exports: [ RouterModule ]
+})
+
+export class AppRoutingModule {}
